e5f3ae29e3fabf0423715b5597afbdf452ce63b1
lokeshyaragarla <lokeshyaragarla@gmail.com>
2025-05-12 11:13:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) return 0; // The 0th Fibonacci number
+    if (num === 1) return 1; // The 1st Fibonacci number
+
+    let a = 0; // F(0)
+    let b = 1; // F(1)
+    let fib = 0;
+
+    for (let i = 2; i <= num; i++) {
+        fib = a + b; // F(n) = F(n-1) + F(n-2)
+        a = b; // Move to the next Fibonacci number
+        b = fib; // Update b to the current Fibonacci number
+    }
+
+    return fib; // Return the nth Fibonacci number
 }
 
 module.exports = fibonacci;
